0fee14b3c6cf2390be63e1290e03fdcf89510c56
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2025-04-18 16:50:22 -0700

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body className="bg-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
