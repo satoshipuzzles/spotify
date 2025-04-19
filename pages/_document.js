4740cf781344b2a61510e068bbb80ec35ca1b151
Puzzles <127057721+satoshipuzzles@users.noreply.github.com>
2025-04-18 17:52:35 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        {/* Properly add fonts in _document.js instead of index.js */}
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body className="bg-gray-900">
         <Main />
